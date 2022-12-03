ff38d4829185384cc542bfd45df1f7022c3cd4cc
Michael K <regallibrarian@gmail.com>
2022-12-03 09:55:32 -0500

    Add useEffect for firebase backend

--- src/App.js
+++ src/App.js
@@ -4,9 +4,19 @@ import Products from "./components/Shop/Products";
 
 import { useSelector } from "react-redux";
 import store from "./store";
+import { useEffect } from "react";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch("https://react-http-dfe22-default-rtdb.firebaseio.com/cart.json", {
+      method: "PUT",
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
 
   return (
     <Layout>
